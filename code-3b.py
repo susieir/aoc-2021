6b91ee401c6f1fddccb6bbbeae69369e8f85787d
susieir <susie@irons.email>
2021-12-15 11:22:13 +0000

    Day 3 part b completed

--- code-3b.py
+++ code-3b.py
@@ -1,35 +1,42 @@
 """ Code to solve aoc-2021 day 3 part b """
 
+# Read input file
+def create_input_list(input_path):
+    with open(input_path, 'r') as fp:
+        input_list = [v.strip() for v in fp]
+    return input_list
+
+def find_rating(input_path, rating_type):
+    """ 
+    Looks for most/least popular bit based on rating type and retains for each position
+    Returns decimal of selected binary character
+    """
+    # Set keep values '0' (least common) or '1' (most common) based on rating_type 
+    if rating_type == 'oxygen':
+        keep_val = '0'
+    elif rating_type == 'co2':
+        keep_val = '1'
+    # Create input list
+    short_list = create_input_list(input_path)
+    # Iterate through positions
+    for i in range(len(short_list[0])):
+        while len(short_list) > 1:
+            # Check for least common bit in given position and retain
+            if sum([1 if v[i] == '1' else 0 for v in short_list]) >= len(short_list)/2:
+                short_list = [v for v in short_list if v[i] == keep_val]
+            else:
+                short_list = [v for v in short_list if v[i] != keep_val]
+            i += 1
+        else:
+            # Return decimal of selected binary value
+            return int(short_list[0], 2)
+
+def main(input_path):
+    """ Finds oxygen and co2 scrubber ratings and returns product """
+    return find_rating(input_path, 'oxygen') * find_rating(input_path, 'co2')
+
 # Set input_path
 input_path = "input-3.csv"
 
-# Read input file
-with open(input_path, 'r') as fp:
-    input_list = [v.strip() for v in fp]
-print(input_list)
-
-def filter_list(input_list, pos):
-    # Initialise lists
-    zeros = []
-    ones = []
-    while len(input_list) > 0:
-        #print(f"List: {input_list}")
-        value = input_list[0]
-        #print(f"Value: {value}")
-        if value[pos] == '0':
-            input_list.remove(value)
-            zeros.append(value)
-        else:
-            input_list.remove(value)
-            ones.append(value)
-        #print(f"Zeros: {zeros}")
-        #print(f"Ones: {ones}")
-    if len(zeros) > len(ones):
-        input_list.extend(zeros)
-    else:
-        input_list.extend(ones)
-    return input_list
-    
-
-print(filter_list(input_list, 0))
-
+# Execute function
+print(main(input_path))

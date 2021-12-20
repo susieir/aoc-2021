5dfed55ec28609a03e412d9917e3180b1c2e64a0
susieir <susie@irons.email>
2021-12-20 18:16:21 +0000

    Day 4 part a completed

---
+++ workings-4a.py
@@ -0,0 +1,35 @@
+# Set input path
+input_path = "input-4.csv"
+
+def format_board(board):
+    """
+    Takes a board in str format
+    returns in list of lists format
+    """
+    # Initialise empty board
+    board_formatted = []
+    # Split into rows
+    rows = [x for x in board.split("\n")]
+    for row in rows:
+        board_formatted.append([n for n in row.split() if n.isdigit()])
+    return board_formatted
+
+# Read file and store data
+with open(input_path, 'r') as fp:
+    data = fp.read()
+# Initialise list of boards
+board_list = []
+# Count number of double line breaks
+board_range = data.count("\n\n")
+for i in range(board_range + 1):
+    # Skip first section of data (numbers)
+    if i == 0:
+        i += 1
+        next
+    # Capture bingo board and append to board list
+    else:
+        board = data.split("\n\n")[i]
+        board_list.append(format_board(board))
+        i += 1
+print(board_list)
+
